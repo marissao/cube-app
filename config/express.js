f806ad8b5076c40b1336463ce935d7180be9d99c
marissao <50351548+marissao@users.noreply.github.com>
2021-08-04 17:16:00 -1000

    Rendered handlebars templates

--- config/express.js
+++ config/express.js
@@ -1,10 +1,35 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 module.exports = (app) => {
+    // Register a Handlebars View Engine Using the Node Package
+    app.engine('handlebars', exphbs({
+        extname: 'handlebars',
+        defaultLayout: null,
+    }));
+
+    app.set('view engine', 'handlebars'); // Looks for files that end with "handlebars"
     
-    //TODO: Setup the view engine
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    app.get('/details', (req, res) => {
+        res.render('details');
+    });
+
+    app.get('/create', (req, res) => {
+        res.render('create');
+    });
+
+    app.get('/about', (req, res) => {
+        res.render('about');
+    });
+
+    app.get('/404', (req, res) => {
+        res.render('404');
+    });
 
     //TODO: Setup the body parser
 
