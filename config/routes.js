03b5b2dce7d03df5e1b1e2c00af28f83d2fed2a2
marissao <50351548+marissao@users.noreply.github.com>
2021-08-05 17:02:54 -1000

    Connected to mongoDB

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 // TODO: Require Controllers...
-
+const Cube = require('../models/Cube');
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('index');
@@ -17,7 +17,26 @@ module.exports = (app) => {
         res.render('about');
     });
 
+    app.get('/create/accessory', (req, res) => {
+        res.render('about');
+    });
+
+    app.get('/about', (req, res) => {
+        res.render('about');
+    });
+
     app.get('/*', (req, res) => {
         res.render('404');
+    });
+
+    app.post('/create', function (req, res) {
+        console.log("This is req.body: ", req.body);
+        let newCube = new Cube(req.body);
+        newCube.save(function (err, newCube) {
+            if (err) return console.error(err);
+            console.log("Entry saved!");
+        });
+
+        res.send('Form submission received');
     });
 };
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const env = process.env.NODE_ENV || 'development';
-
 const config = require('./config/config')[env];
 const app = require('express')();
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://marissa:pass1234word@cluster0.mis4g.mongodb.net/cube-database?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+// const Cube = require('./models/Cube');
 
 require('./config/express')(app);
 require('./config/routes')(app);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("db.open is working");
+});
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
