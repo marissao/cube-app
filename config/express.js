ae1679afca3da1f380ba74b49fb4212ecf9c268b
marissao <50351548+marissao@users.noreply.github.com>
2021-08-05 14:50:35 -1000

    Moved routes to seperate file, fixed css and 404 route

--- config/express.js
+++ config/express.js
@@ -4,37 +4,16 @@ const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     // Register a Handlebars View Engine Using the Node Package
-    app.engine('handlebars', exphbs({
-        extname: 'handlebars',
+    app.engine('hbs', exphbs({
+        extname: 'hbs',
         defaultLayout: null,
     }));
+    app.set('view engine', 'hbs'); // Looks for files that end with "handlebars"
 
-    app.set('view engine', 'handlebars'); // Looks for files that end with "handlebars"
+    // Body Parser
+    app.use(bodyParser.urlencoded({extended: true}));
     
-    app.get('/', (req, res) => {
-        res.render('index');
-    });
-
-    app.get('/details', (req, res) => {
-        res.render('details');
-    });
-
-    app.get('/create', (req, res) => {
-        res.render('create');
-    });
-
-    app.get('/about', (req, res) => {
-        res.render('about');
-    });
-
-    app.get('/404', (req, res) => {
-        res.render('404');
-    });
-
+    // Serve Static Files
     app.use(express.static("./static"));
-
-    //TODO: Setup the body parser
-
-    //TODO: Setup the static files
 
 };
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,23 @@
 // TODO: Require Controllers...
 
 module.exports = (app) => {
-    // TODO...
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    app.get('/details', (req, res) => {
+        res.render('details');
+    });
+
+    app.get('/create', (req, res) => {
+        res.render('create');
+    });
+
+    app.get('/about', (req, res) => {
+        res.render('about');
+    });
+
+    app.get('/*', (req, res) => {
+        res.render('404');
+    });
 };
