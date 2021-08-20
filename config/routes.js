04fe6a65d5b71002161d10e70c01e54acaa6fc38
marissao <50351548+marissao@users.noreply.github.com>
2021-08-19 17:09:55 -1000

    Added routes for Edit and Delete page, created editController

--- config/routes.js
+++ config/routes.js
@@ -2,60 +2,33 @@ const publicController = require('../controllers/publicController');
 const createController = require('../controllers/createController');
 const detailsController = require('../controllers/detailsController');
 const userController = require('../controllers/userController');
+const editController = require('../controllers/editController');
 const session = require('express-session');
 const {auth} = require('../middleware/auth');
 
-module.exports = (app) => {
-    // Express-session: configure session middleware
-    // app.use(
-    //     session({
-    //         key: "user_id",
-    //         secret: "somerandomstuff",
-    //         resave: false,
-    //         saveUninitialized: false,
-    //         cookie: {
-    //             expires: 600000, // Session lasts 6 days
-    //         },
-    //     })
-    // );
-    
-    // Middleware deletes cookies in user browser if user is logged out
-    // app.use((req, res, next) => {
-    //     if (req.session.user && req.cookies.user_id) {
-    //         res.redirect("user_id");
-    //     }
-    //     next();
-    // });
-    
-    // Middleware function which checks whether user is signed in or not. Used to pass into routes.
-    // const sessionChecker = (req, res, next) => {
-    //     console.log("User is logged in!");
-    //     if (req.session.user && req.cookies.user_id) {
-    //         console.log("I'm redirecting b/c of middleware");
-    //         res.redirect("/");
-    //     } else {
-    //         next();
-    //     }
-    // };
-    
-    app.get('/', auth, publicController.index_get);
+module.exports = (app) => {    
+    app.get('/', publicController.index_get);
 
-    app.get('/details/:id', auth, detailsController.details_id_get);
+    app.get('/details/:id', detailsController.details_id_get);
 
-    app.get('/create/cube', auth, createController.create_cube_get);
+    app.get('/create/cube', createController.create_cube_get);
 
-    app.get('/about', auth, publicController.about_get);
+    app.get('/about', publicController.about_get);
 
-    app.get('/create/accessory', auth, createController.create_accessory_get);
+    app.get('/create/accessory', createController.create_accessory_get);
 
-    app.get('/details/attach/accessory/:id', auth, detailsController.details_attach_accessory_get);
+    app.get('/details/attach/accessory/:id', detailsController.details_attach_accessory_get);
 
-    app.get('/login', auth, publicController.login_get);
+    app.get('/login', publicController.login_get);
 
-    app.get('/register', auth, publicController.register_get);
+    app.get('/register', publicController.register_get);
+
+    app.get('/edit', editController.edit_get);
+
+    app.get('/delete', editController.delete_get);
 
     app.get('/*', auth, (req, res) => {
-        res.render('404');
+        res.render('404', {jwt: req.cookies.jwt});
     });
     
     app.post('/create/cube', createController.create_cube_post);
